e9ab0267b705773675aaf4750b4da093729a3879
Michael Perrenoud <info@consultwithmike.us>
2020-04-12 22:30:11 -0600

    TheCode project.

---
+++ Using appsettings.json in .NET Core Console Applications/TheCode/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+
+namespace TheCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // load the app settings into configuration
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            // parse all settings into the Settings class structure
+            var settings = configuration.Get<Settings>();
+
+            // setup logging
+            var services = new ServiceCollection() as IServiceCollection;
+
+            services.AddLogging(configure =>
+            {
+                configure.AddConfiguration(configuration.GetSection("Logging"));
+                configure.AddConsole();
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // log settings that were parsed
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+            logger.LogDebug("Settings: {Settings}", settings);
+
+            // dispose the serviceProvider; this will ensure all logs get flushed
+            serviceProvider.Dispose();
+        }
+    }
+}
---
+++ Using appsettings.json in .NET Core Console Applications/TheCode/Settings.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TheCode
+{
+    public class Settings
+    {
+        public ConnectionStrings ConnectionStrings { get; set; }
+
+        public Logging Logging { get; set; }
+
+        public AppSettings AppSettings { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+        }
+    }
+
+    public class ConnectionStrings
+    {
+
+    }
+
+    public class Logging
+    {
+        public ConsoleSettings Console { get; set; }
+
+        public bool IncludeScopes { get; set; }
+
+        public class ConsoleSettings
+        {
+            public LogLevelSettings LogLevel { get; set; }
+
+            public class LogLevelSettings
+            {
+                [JsonConverter(typeof(JsonStringEnumConverter))]
+                public LogLevel Default { get; set; }
+
+                [JsonConverter(typeof(JsonStringEnumConverter))]
+                public LogLevel System { get; set; }
+
+                [JsonConverter(typeof(JsonStringEnumConverter))]
+                public LogLevel Microsoft { get; set; }
+            }
+        }
+    }
+
+    public class AppSettings
+    {
+
+    }
+}
